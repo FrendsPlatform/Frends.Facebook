8f2876c16589e152b1fd01f7f46f682df8041886
Vertti Ahlstén <vertti.ahlsten@hiq.fi>
2023-11-13 13:06:00 +0200

    Warning resolved

--- Frends.Facebook.Get/Frends.Facebook.Get/Frends.Facebook.Get.cs
+++ Frends.Facebook.Get/Frends.Facebook.Get/Frends.Facebook.Get.cs
@@ -51,11 +51,11 @@ public static class Facebook
             var responseMessage = await client.SendAsync(request, cancellationToken);
             responseMessage.EnsureSuccessStatusCode();
             var responseString = string.Empty;
-            #if NETSTANDARD2_0
+#if NETSTANDARD2_0
             responseString = await responseMessage.Content.ReadAsStringAsync();
-            #elif NET6_0_OR_GREATER
+#elif NET6_0_OR_GREATER
             responseString = await responseMessage.Content.ReadAsStringAsync(cancellationToken);
-            #endif
+#endif
 
             return new Result(true, responseString);
         }
