b365c10345aa803850ad6f1754ed7a41c32a99ea
Vertti Ahlstén <vertti.ahlsten@hiq.fi>
2023-11-27 12:49:08 +0200

    Warnings resolved

--- Frends.Facebook.Get/Frends.Facebook.Get.Tests/GlobalSuppressions.cs
+++ Frends.Facebook.Get/Frends.Facebook.Get.Tests/GlobalSuppressions.cs
@@ -2,6 +2,4 @@
 
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Following Frends documentation guidelines")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Following Frends documentation guidelines", Scope = "type", Target = "~T:Frends.Facebook.Get.Tests.UnitTests")]
-[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Following Frends documentation guidelines", Scope = "member", Target = "~M:Frends.Facebook.Get.Tests.UnitTests.SetUp~System.Threading.Tasks.Task")]
-[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Following Frends documentation guidelines", Scope = "member", Target = "~M:Frends.Facebook.Get.Tests.UnitTests.TestGetOther")]
-[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Following Frends documentation guidelines", Scope = "member", Target = "~M:Frends.Facebook.Get.Tests.UnitTests.TestGetOtherWithDuplicateParameters")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Following Frends documentation guidelines", Scope = "namespaceanddescendants", Target = "~N:Frends.Facebook.Get.Tests")]
